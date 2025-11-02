ecbb80c9616a210c83370f602681cb59641d91dc
sheymaelali94-cmd <seymaelali090@gmail.com>
2025-11-02 22:23:24 +0300

    fix router base name

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom"; // ❌ No more Router here
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
