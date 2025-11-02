adb4877f60e576dc4572359967d5c2d844532460
sheymaelali94-cmd <seymaelali090@gmail.com>
2025-11-02 21:59:04 +0300

    fix path for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Banking-Website/', // Must match your repo name
+  base: "/", // Must match your repo name
 });
